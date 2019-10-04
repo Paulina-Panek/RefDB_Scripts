972cceaec48dd36604149393e988b20c0322ef31
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2019-10-03 17:45:50 -1000

    Add files via upload

--- headers_out.py
+++ headers_out.py
@@ -1,15 +1,23 @@
+########################################################
+# Paulina Panek                                        #
+# Produces file with no headers or empty lines         #
+########################################################
+import string
 counter = 0
 
 with open("RefDB-C.db") as file:
 
     file_contents = file.readlines()
-    file1 = open("no_header_RefDB.txt", "w")
+    print("RefDB len: ", len(file_contents))
+    file1 = open("no_header_RefDB.csv", "w")
     x = True
 
+    print("file_contents len = ", len(file_contents))
+
     while counter < len(file_contents):
-        if not (file_contents[counter].startswith('#') or file_contents[counter].startswith('>')):
-            print(file_contents[counter])
-            print(counter)
+        if not (file_contents[counter].startswith('#') or file_contents[counter].startswith('>') or file_contents[counter].startswith('\n')):
+
             file1.write(file_contents[counter])
 
         counter = counter + 1
+
